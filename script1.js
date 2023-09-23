da1429b671ff5a7529f8ad6b4d27136171cf464d
Sanmit0804 <104015673+Sanmit0804@users.noreply.github.com>
2023-09-23 20:35:26 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,270 @@
+const wrapper = document.querySelector(".wrapper");
+const backBtn = document.querySelector(".back-btn");
+const menuBtn = document.querySelector(".menu-btn");
+
+const toggleScreen = () => {
+  wrapper.classList.toggle("show-category");
+};
+
+menuBtn.addEventListener("click", toggleScreen);
+backBtn.addEventListener("click", toggleScreen);
+
+const addTaskBtn = document.querySelector(".add-task-btn");
+const addTaskForm = document.querySelector(".add-task");
+const blackBackdrop = document.querySelector(".black-backdrop");
+
+const toggleAddTaskForm = () => {
+  addTaskForm.classList.toggle("active");
+  blackBackdrop.classList.toggle("active");
+  addTaskBtn.classList.toggle("active");
+};
+
+addTaskBtn.addEventListener("click", toggleAddTaskForm);
+blackBackdrop.addEventListener("click", toggleAddTaskForm);
+
+// Adding categories and tasks with js
+
+let categories = [
+  {
+    title: "Personal",
+    img: "boy.png",
+  },
+  {
+    title: "Work",
+    img: "briefcase.png",
+  },
+  {
+    title: "Shopping",
+    img: "shopping.png",
+  },
+  {
+    title: "Coding",
+    img: "web-design.png",
+  },
+  {
+    title: "Health",
+    img: "healthcare.png",
+  },
+  {
+    title: "Fitness",
+    img: "dumbbell.png",
+  },
+  {
+    title: "Education",
+    img: "education.png",
+  },
+  {
+    title: "Finance",
+    img: "saving.png",
+  },
+];
+
+let tasks = [
+  {
+    id: 1,
+    task: "Go to market",
+    category: "Shopping",
+    completed: false,
+  },
+  {
+    id: 2,
+    task: "Read a chapter of a book",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 3,
+    task: "Prepare presentation for meeting",
+    category: "Work",
+    completed: false,
+  },
+  {
+    id: 4,
+    task: "Complete coding challenge",
+    category: "Coding",
+    completed: false,
+  },
+  {
+    id: 5,
+    task: "Take a 30-minute walk",
+    category: "Health",
+    completed: false,
+  },
+  {
+    id: 6,
+    task: "Do a 20-minute HIIT workout",
+    category: "Fitness",
+    completed: false,
+  },
+  {
+    id: 7,
+    task: "Watch an educational video online",
+    category: "Education",
+    completed: false,
+  },
+  {
+    id: 8,
+    task: "Review monthly budget",
+    category: "Finance",
+    completed: false,
+  },
+  {
+    id: 9,
+    task: "Buy groceries for the week",
+    category: "Shopping",
+    completed: false,
+  },
+  {
+    id: 10,
+    task: "Write in a journal",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 11,
+    task: "Send follow-up emails",
+    category: "Work",
+    completed: false,
+  },
+  {
+    id: 12,
+    task: "Work on a coding side project",
+    category: "Coding",
+    completed: false,
+  },
+  {
+    id: 13,
+    task: "Try a new healthy recipe",
+    category: "Health",
+    completed: false,
+  },
+  {
+    id: 14,
+    task: "Attend a yoga class",
+    category: "Fitness",
+    completed: false,
+  },
+  {
+    id: 15,
+    task: "Read an article about a new topic",
+    category: "Education",
+    completed: false,
+  },
+  {
+    id: 16,
+    task: "Set up automatic bill payments",
+    category: "Finance",
+    completed: false,
+  },
+  // Additional tasks for each category
+  {
+    id: 17,
+    task: "Buy new clothes",
+    category: "Shopping",
+    completed: false,
+  },
+  {
+    id: 18,
+    task: "Meditate for 10 minutes",
+    category: "Personal",
+    completed: false,
+  },
+  {
+    id: 19,
+    task: "Prepare agenda for team meeting",
+    category: "Work",
+    completed: false,
+  },
+  {
+    id: 20,
+    task: "Debug a software issue",
+    category: "Coding",
+    completed: false,
+  },
+  {
+    id: 21,
+    task: "Try a new recipe for lunch",
+    category: "Health",
+    completed: false,
+  },
+  {
+    id: 22,
+    task: "Go for a run",
+    category: "Fitness",
+    completed: false,
+  },
+  {
+    id: 23,
+    task: "Learn a new language online",
+    category: "Education",
+    completed: false,
+  },
+  {
+    id: 24,
+    task: "Read about history",
+    category: "Education",
+    completed: false,
+  },
+  {
+    id: 25,
+    task: "Review investment portfolio",
+    category: "Finance",
+    completed: false,
+  },
+  // Add more tasks for each category as desired
+];
+
+const categoriesContainer = document.querySelector(".categories");
+
+const renderCategories = () => {
+  categoriesContainer.innerHTML = "";
+  categories.forEach((category) => {
+    // get all the tasks of current category
+    const categoryTasks = tasks.filter(
+      (task) => task.category.toLowerCase() === category.title.toLowerCase()
+    );
+
+    // create a div to render category
+    const div = document.createElement("div");
+    
+    div.classList.add("category");
+    div.addEventListener("click", ()=>{
+        screen.
+    })
+    div.innerHTML = `  
+    <div class="left">
+    <img src="images/${category.img}" alt="${category.title}" />
+    <div class="content">
+      <h1>${category.title}</h1>
+      <p>${categoryTasks.filter}</p>
+    </div>
+  </div>
+
+
+  <div class="options">
+    <div class="toggle-btn">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 6.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 12.75a.75.75 0 110-1.5.75.75 0 010 1.5zM12 18.75a.75.75 0 110-1.5.75.75 0 010 1.5z"
+        />
+      </svg>
+    </div>
+  </div>
+     `;
+
+    
+
+   
+
+    categoriesContainer.appendChild(div);
+  });
+};
